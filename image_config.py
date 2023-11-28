4535922f5fa7abc8b77f67ebecd5759bacbfb6a0
monsterchick <38910509+monsterchick@users.noreply.github.com>
2023-11-28 14:07:54 +0000

    1.0.0
    updates Return File Path feature

---
+++ image_config.py
@@ -0,0 +1,8 @@
+# web page
+img_apply_balance = 'img_apply_balance.png'
+redeem_box = 'redeem_box.png'
+applied = 'applied.png'
+successful = 'successful.png'
+reload = 'reload.png'
+
+# wechat
---
+++ main.py
@@ -0,0 +1,14 @@
+import image_config
+from module import return_abs_file_path
+
+# 圖片名字
+img_apply_balance = image_config.img_apply_balance
+redeem_box = image_config.redeem_box
+img_applied = image_config.applied
+img_successful = image_config.successful
+img_reload = image_config.reload
+
+# 返回圖片絕對路徑
+print(return_abs_file_path.find_file_path(img_successful))
+return_abs_file_path.find_file_path(img_successful)
+
